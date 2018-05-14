bf53c3b62722d94a33eeecd9dee5995b747875a3
Bob Apthorpe <bob.apthorpe@gmail.com>
2018-05-13 20:50:34 -0500

    Added placeholder test file for jeppson.constants

---
+++ tests/test_constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import jeppson.constants as jc
+
+__author__ = "Bob Apthorpe"
+__copyright__ = "Bob Apthorpe"
+__license__ = "mit"
+
